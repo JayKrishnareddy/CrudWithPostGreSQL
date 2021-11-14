b6b8d10a5b28b3900db8194b40b098c347ad2e91
Jay Krishna Reddy <jaykrishnareddy@gmail.com>
2021-11-14 14:22:05 +0530

    Add project files.

---
+++ CrudWithPostGreSQL/Controllers/WeatherForecastController.cs
@@ -0,0 +1,45 @@
+﻿using CrudWithPostGreSQL.Models;
+using Dapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CrudWithPostGreSQL.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {       
+
+        private readonly IConfiguration _configuration;
+        private readonly string _conStr = "myconn";
+
+        public WeatherForecastController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpGet(nameof(GetData))]
+        public async Task<IActionResult> GetData()
+        {
+            List<Department> departmentList = new();
+            string query = @"Select * from Department";
+            string sqlDataSource = _configuration.GetConnectionString(_conStr);
+            using (NpgsqlConnection myCon = new NpgsqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                departmentList = myCon.Query<Department>(query).ToList();
+                myCon.Close();
+            }
+            return Ok(departmentList);
+        }
+    }
+
+    
+}
---
+++ CrudWithPostGreSQL/Models/Employee.cs
@@ -0,0 +1,11 @@
+﻿namespace CrudWithPostGreSQL.Models
+{
+    public class Employee
+    {
+        public int EmployeeId { get; set; }
+        public string EmployeeName { get; set; }
+        public string  Department { get; set; }
+        public string  DateofJoining { get; set; }
+        public string FileName { get; set; }
+    }
+}
